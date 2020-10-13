96569966000ed83dcc90aa3840b2b4da22051f10
Florenceliyi <2820482349@qq.com>
2020-10-13 12:42:05 +0800

    项目搭建

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+// 1 引入 ant 组件
+import { Button } from "antd-mobile";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        {/* 2 使用 */}
+        <div>
+          <Button>按钮</Button>
+        </div>
+      </Fragment>
+    );
+  }
+}
+export default App;
